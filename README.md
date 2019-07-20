61e53a181ac26149942442422d0c8c7e204078a1
rubeus77 <rubeus77@gmail.com>
2019-07-20 12:13:00 +0200

    first files

---
+++ README.md
@@ -0,0 +1,4 @@
+# PHP, MySQL CRUD by OOP
+Project to learn CRUD operaton in PHP connection with MySQL by OOP.
+
+The tutorial is from [Code of a Ninja](https://www.codeofaninja.com/2014/06/php-object-oriented-crud-example-oop.html)
---
+++ layout-header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="PL-pl">
+<head>
+    <meta charset="utf=8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo $page_title; ?></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="./libs/css/custome.css">
+
+</head>
+<body>
+    <div class="container">
+        <?php
+            echo "<div class='page-header'> 
+                <h1>{$page_title}</h1>
+                </div>";
+        ?>
+        
+
+</body>
+</html>
